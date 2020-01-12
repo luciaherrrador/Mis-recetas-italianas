84fc6405cb12db1a6faa523d8708cac44eeb510a
luciaherrrador <luciaherradorcarrasco@gmail.com>
2020-01-12 09:02:46 +0100

    add docs folder

---
+++ docs/precache-manifest.63862e96f2c67c949336f60365374fca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "081c7e5c5d0091a224bc726f6adad489",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bef83be49e4a530d5809",
+    "url": "./static/css/main.833f281b.chunk.css"
+  },
+  {
+    "revision": "3ab4a69c42cb1d318fb3",
+    "url": "./static/js/2.00de3941.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.00de3941.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bef83be49e4a530d5809",
+    "url": "./static/js/main.7ece1531.chunk.js"
+  },
+  {
+    "revision": "cba9b9eee6ec49dc9d8c",
+    "url": "./static/js/runtime-main.9a8f124a.js"
+  }
+]);
